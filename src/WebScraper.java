8af9c678bab77e0fe276f9e2133850286a053d32
nyw18 <nathan.wang18@imperial.ac.uk>
2019-11-01 20:04:12 +0000

    Implemented a regex pattern matcher

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -2,25 +2,47 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebScraper {
 
-  private final int MAX_URLS = 100;
-  private int printed = 0;
+  private static final int MAX_URLS = 100;
+  private static int urlListSize;
+  private static Set<String> urlSet;
+  private static List<String> searchList;
 
   public static void main(String[] args) {
+    // check args are valid
     if (args.length != 1) {
       System.out.println("Wrong number of parameters.");
       return;
     }
+    init(args[0]);
+    // while searchList != empty and set < MAX_URLS
+    // --get next from searchList and get more urls
+    //while end
+    //print first MAX_URLS urls
+
     try {
       String startUrl = args[0];
       URL url = new URL(startUrl);
-      System.out.println(getPageContent(url));
+      String pageContent = getPageContent(url);
+      if (pageContent != null) {
+        printLinks(pageContent);
+        //System.out.println(pageContent);
+      }
     } catch (MalformedURLException e) {
       System.out.println("Invalid start Url passed.");
     }
+  }
+
+  private static void init(String url) {
+    urlSet = new HashSet<>();
+    searchList = new ArrayList<>();
+    searchList.add(url);
+    urlListSize = 0;
   }
 
   private static String getPageContent(URL url) {
@@ -32,5 +54,27 @@ public class WebScraper {
       System.out.println("Error getting page content.");
     }
     return null;
+  }
+
+  private static void printLinks(String page) {
+    // split lines with ">"
+    // search for <a start and then find href=....
+    // put url in list and set if unique and printed++
+    String[] lines = page.split(">");
+    Pattern linkPattern = Pattern.compile(".*<a.+href=\"([^\\\"]+)\"");
+    for(String line : lines) {
+      if (urlListSize > MAX_URLS) {
+        break;
+      }
+      //todo: replace check with pattern matcher
+      if (line.matches(".*<a.+href=\"([^\\\"]+)\".*")) {
+
+        Matcher linkMatcher = linkPattern.matcher(line);
+        linkMatcher.find();
+        System.out.println(linkMatcher.group(1));
+      }
+
+
+    }
   }
 }
